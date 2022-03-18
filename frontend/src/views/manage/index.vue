23097fcb896671ab0e5959919d141db34be19278
wweihuayao <903429061@qq.com>
2022-03-18 18:59:22 +0800

    bugfix:表单问题间隔24px

--- frontend/src/views/manage/index.vue
+++ frontend/src/views/manage/index.vue
@@ -8,3 +8,20 @@ export default {
   name: 'ManageHome',
 };
 </script>
+<style lang="postcss">
+.manage-page-container{
+  .bk-form  {
+    .bk-form-item {
+      margin-top: 24px !important;
+    }
+  }
+
+  .bk-dialog-body{
+    .bk-form-item {
+      margin-top: 0 !important;
+      margin-bottom: 24px !important;
+    }
+  }
+}
+
+</style>
